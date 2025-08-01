2cf945000c205b73afe6a6b291bfaf0f012c4133
Jarry1222 <William-Champion@outlook.com>
2025-08-01 17:18:15 -0300

    w

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,18 @@
+import { useColorScheme } from '@/hooks/useColorScheme';
+import { useThemeColor } from '@/hooks/useThemeColor';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
+import { Text } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import "../global.css";
 
 export default function RootLayout() {
+  const backgroundColor = useThemeColor(
+    {
+      light : "red" , dark : "indigo"
+    }, "background"
+  )
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -18,12 +24,10 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
+   <GestureHandlerRootView>
+     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Text className='text-3xl mt-20 text-center mr-5'>Inicio de la Vida</Text>
     </ThemeProvider>
+   </GestureHandlerRootView>
   );
 }
